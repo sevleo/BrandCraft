be2bd49143f4f46b5ce04e4cfdd77ff9f36b76cb
Seva <seva.leonov@hotmail.com>
2025-03-04 10:16:48 +0700

    Update gtag

--- frontend-nuxt/app.vue
+++ frontend-nuxt/app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { useHead } from "@unhead/vue";
+
+useHead({
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-VHPLXRGRB6",
+      async: true,
+      defer: true, // Loads after other critical resources
+    },
+    {
+      children: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VHPLXRGRB6');
+      `,
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <NuxtPage />
